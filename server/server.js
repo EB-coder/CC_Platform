905b97929ae256887e1413973273464b229d5c75
Bezpalevo07 <bekmurzaev2227kg@gmail.com>
2025-05-24 03:54:14 +0600

    Add root route to serve index.html

--- server/server.js
+++ server/server.js
@@ -42,6 +42,11 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Serve index.html for root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // Database configuration - use DATABASE_URL for production
 // Отключаем SSL проверку для DigitalOcean
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
